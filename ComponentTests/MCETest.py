8bde4f36e281e39c67c1bb74e1e0928bb5a9de01
Ammar Bahman <bahman.ammar@gmail.com>
2018-07-26 12:54:44 -0400

    correct comment spacing

--- ComponentTests/MCETest.py
+++ ComponentTests/MCETest.py
@@ -1,3 +1,4 @@
+from __future__ import division, print_function, absolute_import
 from ACHP.FinCorrelations import FinInputs
 from ACHP.Evaporator import EvaporatorClass
 from ACHP.MultiCircuitEvaporator import MultiCircuitEvaporatorClass
@@ -35,7 +36,7 @@ Ref = 'R410A'
 Backend = 'TTSE&HEOS' #choose between: 'HEOS','TTSE&HEOS','BICUBIC&HEOS','REFPROP','SRK','PR'
 AS = CP.AbstractState(Backend, Ref)
 
-#This uses the normal baseline evaporator model
+# This uses the normal baseline evaporator model
 kwargs={'AS': AS,
         'mdot_r': 0.0708,
         'psat_r': PropsSI('P','T',282.0,'Q',1.0,Ref),
@@ -48,7 +49,7 @@ Evap=EvaporatorClass(**kwargs)
 Evap.Calculate()
 print ('Evap Q=' + str(Evap.Q) + ' W')
 
-#This uses the multi-circuited evaporator model but with no mal-distribution
+# This uses the multi-circuited evaporator model but with no mal-distribution
 kwargs={'AS': AS,
         'mdot_r': 0.0708,
         'psat_r': PropsSI('P','T',282.0,'Q',1.0,Ref),
@@ -61,7 +62,7 @@ MCE=MultiCircuitEvaporatorClass(**kwargs)
 MCE.Calculate()
 print ('MCE Q='+str(MCE.Q)+' W w/o mal-distribution')
 
-#Not exactly the same since
+# Not exactly the same since
 # This uses the multi-circuited evaporator model with mal-distribution of 
 # refrigerant, refrigerant quality, and air volumetric flow rate 
 kwargs={'AS': AS,
@@ -80,8 +81,8 @@ MCE.Calculate()
 print ('MCE Q='+str(MCE.Q)+' W w/ mal-distribution')
 
 
-#Another way to express air maldirtribution for the last example, 
-#using a vector of Vdot_ha and m_dot_r instead of vector of coefficients
+# Another way to express air maldirtribution for the last example, 
+# using a vector of Vdot_ha and m_dot_r instead of vector of coefficients
 FinsTubes.Air.Vdot_ha=0.5663*np.array([0.3,0.2,0.2,0.2,0.1])
 kwargs={'AS': AS,
         'mdot_r': 0.0708*np.array([0.3,0.2,0.1,0.2,0.2]),
